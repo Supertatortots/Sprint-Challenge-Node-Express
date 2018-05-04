20fd6b56ce9a01a468abb3bd9f5f73fddaa7c1c8
totsuya <supertatortots@gmail.com>
2018-05-04 13:31:44 -0400

    fixed filepath issue

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
-
+const helmet = require ('helmet');
 const server = express();
+const cors = require ('cors');
 
-server.get('/', (req, res) => {
-  res.send('Testing One Two Three!');
-});
+const actionRoute = require('./data/routes/actionRoute');
+const projectRoute = require('./data/routes/projectRoute')
+
+
+
+// server.get('/', (req, res) => {
+//   res.send('Testing One Two Three!');
+// });
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+
+
 
 server.listen(5000, () => console.log("\n== API Running on port 5000 ==\n"));
